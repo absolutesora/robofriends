56588c10cf819509727768c362471db13a270684
taka <taka@austlink.com.au>
2020-03-11 15:42:35 +1100

    Updates

---
+++ precache-manifest.6a10aa7fe03d30dcff44f76d556dd992.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1074a92ad46372c1c6d339521302ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f9d1b9d0ea8f31bad2f",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "965b9fef3a9506041195",
+    "url": "/static/css/main.65a3e0c2.chunk.css"
+  },
+  {
+    "revision": "8f9d1b9d0ea8f31bad2f",
+    "url": "/static/js/2.5b396b40.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.5b396b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965b9fef3a9506041195",
+    "url": "/static/js/main.a34640be.chunk.js"
+  },
+  {
+    "revision": "29247c02f79741d4d17e",
+    "url": "/static/js/runtime-main.44525653.js"
+  }
+]);
